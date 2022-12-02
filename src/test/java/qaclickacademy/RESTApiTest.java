57be07510bccc4339e0b56c42d7e6cb36c25a096
Vinaykumar79 <qavinaykumargs@gmail.com>
2022-12-02 14:16:55 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/RESTApiTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("PostJira");
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	// How to execute all test case from Test folder using  mvn test command 
+	
+	// TestNG xml file from Maven.
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void ElementUi() {
+		System.out.println("ElementUi");
+	}
+}
